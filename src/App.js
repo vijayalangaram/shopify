203be9ae0bf4f35cf4a1749e97205c1f47691f02
vijayalangaram <vijayalangaram@gmail.com>
2020-11-20 13:21:47 +0530

    reposrtpage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,311 @@
 import './App.css';
 // import './shopify.html';
 // import Viewico from './viewico.png';
-import Shopifyscrppage from './Shopifyscrppage';
+// import Shopifyscrppage from './Shopifyscrppage';
 
 function App() {
   return (    
-    <div className="App" >  
+    <div className="App" >          
+        {/* <Shopifyscrppage/> */}
+
+       
+       <div className="Reporttitle">
+         <h1> Report </h1>      
+         </div>       
         
-        <Shopifyscrppage/>
+         <div >
+         <h1>  </h1>         
+         </div>
+
+
+<table className="Imagesscanned" width="60%" height = "80" border="1"  bardercolor="black" align="center" >
+<tr>    
+<td>       
+<div>
+<h2 align="center">2000+<br/> <span>Images scanned</span> </h2> 
+</div>
+</td>
+<td>
+<div>
+<h2 align="center">183 <br/> Infringement Found</h2> 
+</div>
+</td>
+<td>
+<div> 
+<button style={{height: "32px",width: "150px", color: "blue", backgroundColor: "white", borderColor: "blue" }} >Download Report</button>
+</div>
+</td>      
+</tr>
+</table>
+
+         <div >
+         <h1>  </h1> 
+                
+         </div>
+
+
+
+
+<table height="80"  width="50%" bardercolor="white" align="center">
+
+<tr> 
+<td>
+<div className="card_text">
+<img src="C:\Users\Corona\Desktop\sho\my-new-app\src\pics\viewico.png"  height="55" width="45" />
+<p>Your Store </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<img src="C:\Users\Corona\Desktop\sho\my-new-app\src\pics\viewico.png"  height="55" width="45" />
+<p>Match Found </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<p>Matched on <br/> <u>Carbon38.com</u> </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<p>99% </p>
+</div>
+</td>
+
+<td>
+   <div> 
+<button align="center">View Product</button>
+ </div>
+</td>  
+</tr>
+
+<tr>    
+<td>
+<div className="card_text">
+<img src="C:\Users\Corona\Desktop\sho\my-new-app\src\pics\viewico.png"  height="55" width="45" />
+<p>Your Store </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<img src="C:\Users\Corona\Desktop\sho\my-new-app\src\pics\viewico.png"  height="55" width="45" />
+<p>Match Found</p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<p>Matched on <br/> <u>H&M.com</u> </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<p>98% </p>
+</div>
+</td>
+
+<td>
+   <div> 
+<button align="center">View Product</button>
+ </div>
+</td>  
+
+
+</tr>
+
+
+<tr>    
+<td>
+<div className="card_text">
+<img src="C:\Users\Corona\Desktop\sho\my-new-app\src\pics\viewico.png"  height="55" width="45" />
+<p>Your Store </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<img src="C:\Users\Corona\Desktop\sho\my-new-app\src\pics\viewico.png"  height="55" width="45" />
+<p>Match Found</p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<p>Matched on <br/> <u>Nike.com</u> </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<p>95% </p>
+</div>
+</td>
+
+<td>
+   <div> 
+<button align="center">View Product</button>
+ </div>
+</td>  
+
+
+</tr>
+
+
+
+<tr>    
+<td>
+<div className="card_text">
+<img src="C:\Users\Corona\Desktop\sho\my-new-app\src\pics\viewico.png"  height="55" width="45" />
+<p>Your Store </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<img src="C:\Users\Corona\Desktop\sho\my-new-app\src\pics\viewico.png"  height="55" width="45" />
+<p>Match Found </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<p>Matched on <br/> <u>Uniqlo.com</u> </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<p>85% </p>
+</div>
+</td>
+
+<td>
+   <div> 
+<button align="center">View Product</button>
+ </div>
+</td>  
+
+
+</tr>
+
+<tr>    
+<td>
+<div className="card_text">
+<img src="C:\Users\Corona\Desktop\sho\my-new-app\src\pics\viewico.png"  height="55" width="45" />
+<p>Your Store </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<img src="C:\Users\Corona\Desktop\sho\my-new-app\src\pics\viewico.png"  height="55" width="45" />
+<p>Match Found </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<p>Matched on <br/> <u>Ugmonk.com</u> </p>
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<p>83% </p>
+</div>
+</td>
+
+<td>
+   <div> 
+<button align="center">View Product</button>
+ </div>
+</td> 
+</tr>
+</table>
+
+
+<table height="80" align="center" >
+<tr>  
+
+<td>
+<div className="card_text">
+<img src="C:\Users\Corona\Desktop\sho\my-new-app\src\pics\viewico.png"  height="55" width="45" />
+</div>
+</td>
+
+<td>
+<div className="card_text">
+<p>Enter payment details to unlcok <br/> 2000+ images from your store </p>
+</div>
+</td>
+
+<td>
+   <div className="buttong"> 
+<button style={{height: "32px",width: "150px", left: "82px", color: "white", backgroundColor: "blue", borderColor: "blue"}}>Unlock All Results</button>
+ </div>
+</td> 
+
+
+
+</tr>
+</table>
+
+
+<table height="80" align="center" >
+<tr>  
+
+<td>
+   <div> 
+<button>1</button>
+ </div>
+</td> 
+
+<td>
+   <div> 
+<button>2</button>
+ </div>
+</td> 
+
+
+<td>
+   <div> 
+<button>3</button>
+ </div>
+</td> 
+
+
+<td>
+   <div> 
+<button>4</button>
+ </div>
+</td> 
+
+<td>
+   <div> 
+<button>5</button>
+ </div>
+</td> 
+
+
+</tr>
+</table>
+
+
+
+<div>
+    <p align="center">Since we're not affiliated with Shopify,If your store still gets <br/>shut down you can contact Shopify Support </p>
+</div>
+
+     
+
+
+    
+
       
           {/* <div style={{backgroundColor: 'blue' }}>   
 
@@ -65,5 +363,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
